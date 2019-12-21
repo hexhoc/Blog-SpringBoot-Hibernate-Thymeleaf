fcfa3c13e8a20c4542785d4e1622710aa37133de
hexhoc <hexhoc@gmail.com>
2019-12-21 12:00:01 +0300

    initial commit

---
+++ src/main/java/com/hexhoc/springbootblog/article/Article.java
@@ -0,0 +1,197 @@
+package com.hexhoc.springbootblog.article;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "subUrl")
+    private String subUrl;
+
+    @Column(name = "cover_image")
+    private String coverImage;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @ManyToMany
+    @JoinTable(name="articles_tags_relation",
+            joinColumns = @JoinColumn(name="article_id", referencedColumnName="id"),
+            inverseJoinColumns = @JoinColumn(name="tag_id", referencedColumnName="id")
+    )
+    private Set<Tag> tags;
+
+    @Column(name = "status")
+    private Byte status;
+
+    @Column(name = "views")
+    private Long views;
+
+    @Column(name = "enable_comment")
+    private Byte enableComment;
+
+    @Column(name = "is_deleted")
+    private Byte isDeleted;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @Column(name = "create_time")
+    private LocalDate createTime;
+
+    @Column(name = "update_time")
+    private LocalDate updateTime;
+
+
+    ////////////////////////////
+    //GETTER AND SETTER
+    ////////////////////////////
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSubUrl() {
+        return subUrl;
+    }
+
+    public void setSubUrl(String subUrl) {
+        this.subUrl = subUrl;
+    }
+
+    public String getCoverImage() {
+        return coverImage;
+    }
+
+    public void setCoverImage(String coverImage) {
+        this.coverImage = coverImage;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Set<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+
+    public Long getViews() {
+        return views;
+    }
+
+    public void setViews(Long views) {
+        this.views = views;
+    }
+
+    public Byte getEnableComment() {
+        return enableComment;
+    }
+
+    public void setEnableComment(Byte enableComment) {
+        this.enableComment = enableComment;
+    }
+
+    public Byte getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Byte isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public LocalDate getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDate createTime) {
+        this.createTime = createTime;
+    }
+
+    public LocalDate getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(LocalDate updateTime) {
+        this.updateTime = updateTime;
+    }
+
+
+    ////////////////////////////
+    //OVERRIDE METHODS
+    ////////////////////////////
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", blogId=").append(blogId);
+        sb.append(", blogTitle=").append(blogTitle);
+        sb.append(", blogSubUrl=").append(blogSubUrl);
+        sb.append(", blogCoverImage=").append(blogCoverImage);
+        sb.append(", blogCategoryId=").append(blogCategoryId);
+        sb.append(", blogCategoryName=").append(blogCategoryName);
+        sb.append(", blogTags=").append(blogTags);
+        sb.append(", blogStatus=").append(blogStatus);
+        sb.append(", blogViews=").append(blogViews);
+        sb.append(", enableComment=").append(enableComment);
+        sb.append(", isDeleted=").append(isDeleted);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", updateTime=").append(updateTime);
+        sb.append(", blogContent=").append(blogContent);
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return this.id.hashCode();
+    }
+}
