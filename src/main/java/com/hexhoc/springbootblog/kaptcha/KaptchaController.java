b4e2b35a21f53c7039b516df038ce2d3301fe27a
hexhoc <hexhoc@gmail.com>
2020-02-16 23:51:01 +0300

    Added kaptcha

---
+++ src/main/java/com/hexhoc/springbootblog/kaptcha/KaptchaController.java
@@ -0,0 +1,46 @@
+package com.hexhoc.springbootblog.kaptcha;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+
+@Controller
+public class KaptchaController {
+
+    @Autowired
+    private DefaultKaptcha captchaProducer;
+
+    @GetMapping("/common/kaptcha")
+    public void defaultKaptcha(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        byte[] captchaOutputStream = null;
+        ByteArrayOutputStream imgOutputStream = new ByteArrayOutputStream();
+        try {
+            //Produce the verification code string and save it in the session
+            String verifyCode = captchaProducer.createText();
+            httpServletRequest.getSession().setAttribute("verifyCode", verifyCode);
+            BufferedImage challenge = captchaProducer.createImage(verifyCode);
+            ImageIO.write(challenge, "jpg", imgOutputStream);
+        } catch (IllegalArgumentException e) {
+            httpServletResponse.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        captchaOutputStream = imgOutputStream.toByteArray();
+        httpServletResponse.setHeader("Cache-Control", "no-store");
+        httpServletResponse.setHeader("Pragma", "no-cache");
+        httpServletResponse.setDateHeader("Expires", 0);
+        httpServletResponse.setContentType("image/jpeg");
+        ServletOutputStream responseOutputStream = httpServletResponse.getOutputStream();
+        responseOutputStream.write(captchaOutputStream);
+        responseOutputStream.flush();
+        responseOutputStream.close();
+    }
+
+}
